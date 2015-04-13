38d907a21ecf3780ab1606d61c6d9caf4feaff89
Richard Keenan <richard.keenan@flextrade.com>
2015-04-13 17:56:32 +0100

    Improved error message shown if JFixture is unable to create a type. This fixes #19

--- jfixture/src/main/java/com/flextrade/jfixture/behaviours/noresolution/ThrowingNoResolutionHandler.java
+++ jfixture/src/main/java/com/flextrade/jfixture/behaviours/noresolution/ThrowingNoResolutionHandler.java
@@ -11,9 +11,11 @@ class ThrowingNoResolutionHandler implements NoResolutionHandler {
 
     private String errorMessage(Object request, List<Object> recordedRequests) {
         StringBuilder msg = new StringBuilder();
-        msg.append("JFixture was unable to create an instance from ")
-                .append(request)
-                .append(", most likely because it has no public constructor, is an abstract or non-public type or has no static factory methods")
+        msg.append("JFixture was unable to create an instance of ")
+                .append(request)  .append("\n")
+                .append("Most likely because it has no public constructor, is an abstract or non-public type or has no static factory methods.").append("\n")
+                .append("If this isn't the case it's likely that all constructors and factory methods on the type have thrown an exception.").append("\n")
+                .append("To view any thrown exceptions just add the Tracing Customisation to the JFixture instance, e.g. fixture.customise(new TracingCustomisation(System.out));").append("\n")
                 .append("\n");
 
         for (Object r : recordedRequests) {
